7585ae707dbac3ae587a255ba8393da5efc2755d
ChesterLuna <chester99124@gmail.com>
2024-12-04 23:46:16 -0700

    Create MainButler.py

---
+++ Butler/MainButler.py
@@ -0,0 +1,13 @@
+import Recognizer as rn
+import Speech
+
+
+def main():
+    # Receive information from Recognizer
+    # If a face is found, label it and return the name
+    # Use the Speech to greet the person using the label
+    pass
+
+
+if __name__ == "__main__":
+    main()
