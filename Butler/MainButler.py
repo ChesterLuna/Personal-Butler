dfb2e5c865a0ff5437e6d662fb8ae109bb517009
ChesterLuna <chester99124@gmail.com>
2024-12-09 23:57:31 -0700

    Main loop

--- Butler/MainButler.py
+++ Butler/MainButler.py
@@ -1,12 +1,18 @@
 import Recognizer as rn
 import Speech
 
-
 def main():
     # Receive information from Recognizer
     # If a face is found, label it and return the name
     # Use the Speech to greet the person using the label
-    pass
+    faceFound = None
+    speaker = Speech.Speaker()
+    while faceFound is not False:
+        faceFound = rn.recognizeFace()
+        faceFile, name, data = faceFound
+
+        speaker.greet(name)
+
 
 
 if __name__ == "__main__":
